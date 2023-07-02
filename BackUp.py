d4ac71623c7635637ed32e48c2168b00c87a6d09
jasser ABR <118571919+Jasserabedrazzek@users.noreply.github.com>
2023-07-02 17:04:47 +0100

    Update BackUp.py

--- BackUp.py
+++ BackUp.py
@@ -5,7 +5,7 @@ query_params = st.experimental_get_query_params()
 uniq_id = query_params.get('uniqID', [''])[0]
 if not uniq_id:
     # Provide a default value or handle the missing parameter here
-    st.markdown(f"uniqID not provided , [Go to login page](https://free-storage.streamlit.app/)")
+    st.markdown(f"uniqID not provided , [Go to login page](https://free-storage.streamlit.app/) .")
 else:
     # Display the value of 'uniqID'
     st.write("uniqID:", uniq_id)
