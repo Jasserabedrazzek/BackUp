bb95087f716333dc0a0ef0e0d11e020c51fafd6a
jasser ABR <118571919+Jasserabedrazzek@users.noreply.github.com>
2023-07-02 16:56:19 +0100

    Update BackUp.py

--- BackUp.py
+++ BackUp.py
@@ -4,7 +4,13 @@ query_params = st.experimental_get_query_params()
 # Get the value of the 'uniqID' parameter
 uniq_id = query_params.get('uniqID', [''])[0]
 if not uniq_id:
-    # Redirect to the specified URL
-    st.redirect('https://free-storage.streamlit.app/')
+    # Redirect to the specified URL using HTML
+    st.components.v1.html(
+        """
+        <script>
+        window.location.href = "https://free-storage.streamlit.app/";
+        </script>
+        """
+    )
 # Display the value of 'uniqID'
 st.write("uniqID:", uniq_id)
