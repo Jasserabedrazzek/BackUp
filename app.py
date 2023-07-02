7af8c4b03741279d175e17c297866ce642ca17e4
jasser ABR <118571919+Jasserabedrazzek@users.noreply.github.com>
2023-07-02 16:28:03 +0100

    Update app.py

--- app.py
+++ app.py
@@ -89,8 +89,8 @@ with login:
             with open(filename, "r") as user :
                 account = json.load(user)
             if password == account['Password']:
-                url = f'https://backup-free.streamlit.app/?uniqID={account["uniqID"]}'
-                st.markdown(f"[Go to backup](url)")
+                url = f'https://backup-free.streamlit.app/'
+                st.experimental_set_query_params(uniqID=account["uniqID"])
             
             
             else:
