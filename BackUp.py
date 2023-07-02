4632e4e103156cf145a39fa0adf788fb5cb190a9
jasser ABR <118571919+Jasserabedrazzek@users.noreply.github.com>
2023-07-02 16:52:22 +0100

    Update BackUp.py

--- BackUp.py
+++ BackUp.py
@@ -1,1 +1,8 @@
 import streamlit as st
+query_params = st.experimental_get_query_params()
+
+# Get the value of the 'uniqID' parameter
+uniq_id = query_params.get('uniqID', [''])[0]
+
+# Display the value of 'uniqID'
+st.write("uniqID:", uniq_id)
