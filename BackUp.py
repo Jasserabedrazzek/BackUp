b1e49f08e53e6ed4b79b45246ebc1a2e1b5c451a
jasser ABR <118571919+Jasserabedrazzek@users.noreply.github.com>
2023-07-03 18:00:35 +0100

    Update BackUp.py

--- BackUp.py
+++ BackUp.py
@@ -8,3 +8,8 @@ email = url.get('email', [''])[0]
 # Display the values
 st.write('uniqID:', uniqID)
 st.write('email:', email)
+st.sidebar.title("Sidebar")
+
+# Add different elements to the sidebar
+st.sidebar.text("This is the sidebar.")
+st.sidebar.button("Click me!")
