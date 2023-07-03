b17606eee3fae78e898eb5be841b7e6bb7837d95
jasser ABR <118571919+Jasserabedrazzek@users.noreply.github.com>
2023-07-04 00:23:47 +0100

    Update BackUp.py

--- BackUp.py
+++ BackUp.py
@@ -5,6 +5,7 @@ url = st.experimental_get_query_params()
 uniqID = url.get('uniqID', [''])[0]
 email = url.get('email', [''])[0]
 Name = url.get('name',[''])[0]
+Lname = url.get('Lname',[''])[0]
 
 st.set_page_config(
     page_title="Chat with developers",
@@ -31,8 +32,10 @@ if email and uniqID:
     Id = uniqID
     st.sidebar.title("User")
     st.sidebar.text(f"Name : {Name}")
+    st.sidebar.text(f"Last name : {Lname}")
     st.sidebar.text(f"Email : {email}")
     st.sidebar.text(f"Id : {Id}")
+    
     
     selected_options = st.sidebar.multiselect(
         'Select options',
