ff7ef5ef55aa42d21886e6f071951b0b42f5312d
jasser ABR <118571919+Jasserabedrazzek@users.noreply.github.com>
2023-07-03 17:39:28 +0100

    Update BackUp.py

--- BackUp.py
+++ BackUp.py
@@ -1,6 +1,6 @@
 import streamlit as st
 query_params = st.experimental_get_query_params()
 uniqID = query_params.get('uniqID', [''])[0]
-email = query_params.get('email', [''])[1]
+email = query_params.get('email', [''])[0]
 st.write("uniqID:", uniqID)
 st.write("email:", email)
