77455fa899ec8f1ac54c179557c7b4cba0199f32
jasser ABR <118571919+Jasserabedrazzek@users.noreply.github.com>
2023-07-03 18:13:23 +0100

    Update BackUp.py

--- BackUp.py
+++ BackUp.py
@@ -4,10 +4,12 @@ import streamlit as st
 url = st.experimental_get_query_params()
 uniqID = url.get('uniqID', [''])[0]
 email = url.get('email', [''])[0]
+Name = url.get('name',[''])[0]
 
 if email and uniqID:
   Id = uniqID
   st.sidebar.title("user")
+  st.sidebar.text(f"Name : {name}")
   st.sidebar.text(f"Email : {email}")
   st.sidebar.text(f"Id : {Id}")
 
