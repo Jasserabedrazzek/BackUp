22bfe742b596b5b3b31d345f421fef73d04bc579
jasser ABR <118571919+Jasserabedrazzek@users.noreply.github.com>
2023-07-03 18:22:18 +0100

    Update BackUp.py

--- BackUp.py
+++ BackUp.py
@@ -6,6 +6,27 @@ uniqID = url.get('uniqID', [''])[0]
 email = url.get('email', [''])[0]
 Name = url.get('name',[''])[0]
 
+st.set_page_config(
+    page_title="Chat with devoloppers",
+    page_icon="",
+    layout="centered",
+    initial_sidebar_state="expanded",
+    
+)
+languages = [
+    "Python",
+    "Java",
+    "JavaScript",
+    "C++",
+    "C#",
+    "Ruby",
+    "Go",
+    "Rust",
+    "Swift",
+    "Kotlin",
+    "TypeScript"
+]
+
 if email and uniqID:
   Id = uniqID
   st.sidebar.title("user")
@@ -14,7 +35,7 @@ if email and uniqID:
   st.sidebar.text(f"Id : {Id}")
   selected_options = st.sidebar.multiselect(
     'Select options',
-    ['python', 'js']
+    languages[0]
 )
 
 
