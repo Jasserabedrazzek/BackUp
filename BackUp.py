260ccd682dc370b2852f4d91d0b6f554de81d156
jasser ABR <118571919+Jasserabedrazzek@users.noreply.github.com>
2023-07-03 23:44:41 +0100

    Update BackUp.py

--- BackUp.py
+++ BackUp.py
@@ -7,12 +7,12 @@ email = url.get('email', [''])[0]
 Name = url.get('name',[''])[0]
 
 st.set_page_config(
-    page_title="Chat with devoloppers",
+    page_title="Chat with developers",
     page_icon="",
     layout="centered",
-    initial_sidebar_state="expanded",
-    
+    initial_sidebar_state="expanded"
 )
+
 languages = [
     "Python",
     "Java",
@@ -28,18 +28,17 @@ languages = [
 ]
 
 if email and uniqID:
-      Id = uniqID
-      st.sidebar.title("user")
-      st.sidebar.text(f"Name : {Name}")
-      st.sidebar.text(f"Email : {email}")
-      st.sidebar.text(f"Id : {Id}")
-      selected_options = st.sidebar.multiselect(
+    Id = uniqID
+    st.sidebar.title("User")
+    st.sidebar.text(f"Name: {Name}")
+    st.sidebar.text(f"Email: {email}")
+    st.sidebar.text(f"Id: {Id}")
+    
+    selected_options = st.sidebar.multiselect(
         'Select options',
-        languages, default=languages[0]
-     )
-     ShowName = st.checkbox('Show name.')
-     ShowId = st.checkbox('Show ID.')
-
-
-
-
+        languages,
+        default=languages[0]
+    )
+    
+    ShowName = st.checkbox('Show name.')
+    ShowId = st.checkbox('Show ID.')
