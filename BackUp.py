22b8d1284f7b9acbaac08067179647667847749a
jasser ABR <118571919+Jasserabedrazzek@users.noreply.github.com>
2023-07-04 01:18:47 +0100

    Update BackUp.py

--- BackUp.py
+++ BackUp.py
@@ -6,12 +6,12 @@ import os
 url = st.experimental_get_query_params()
 uniqID = url.get('uniqID', [''])[0]
 email = url.get('email', [''])[0]
-Name = url.get('name',[''])[0]
-Lname = url.get('Lname',[''])[0]
+Name = url.get('name', [''])[0]
+Lname = url.get('Lname', [''])[0]
 
 st.set_page_config(
     page_title="Chat with developers",
-    page_icon="	:computer:",
+    page_icon=":computer:",
     layout="centered",
     initial_sidebar_state="expanded"
 )
@@ -29,44 +29,45 @@ languages = [
     "Kotlin",
     "TypeScript"
 ]
-def MakeFileJson(IDuser , Name , LastName , Email ):
+
+def MakeFileJson(IDuser, Name, LastName, Email):
     UserInformations = {
-        "Name" : Name ,
-        "Last Name" : LastName ,
-        "Email" : Email ,
-        "messages" : ""
+        "Name": Name,
+        "Last Name": LastName,
+        "Email": Email,
+        "messages": ""
     }
     JsonFile = f"{str(IDuser)}.json"
-    try :
-        if os.path.exists(JsonFile):
-            pass
-        else:
-            with open(JsonFile , "w") as SaveUserFile :
-                json.dumps(UserInformations)
-            
-    except :
-        pass
-    
+    try:
+        if not os.path.exists(JsonFile):
+            with open(JsonFile, "w") as SaveUserFile:
+                json_data = json.dumps(UserInformations)
+                SaveUserFile.write(json_data)
+    except Exception as e:
+        st.error(f"Error creating JSON file: {e}")
+
 if uniqID:
     Id = uniqID
     JsonFile = f"{str(Id)}.json"
-    MakeFileJson(Id , Name , Lname, email )
+    MakeFileJson(Id, Name, Lname, email)
     st.sidebar.title("User")
-    st.sidebar.text(f"Name : {Name}")
-    st.sidebar.text(f"Last name : {Lname}")
-    st.sidebar.text(f"Email : {email}")
-    st.sidebar.text(f"Id : {Id}")
-    
-    
-    
+    st.sidebar.text(f"Name: {Name}")
+    st.sidebar.text(f"Last name: {Lname}")
+    st.sidebar.text(f"Email: {email}")
+    st.sidebar.text(f"Id: {Id}")
+
     selected_options = st.sidebar.multiselect(
         'Select options',
         languages,
         default=languages[0]
     )
-    with open(JsonFile, "r") as f :
-        st.write(json.load(f))
+
+    try:
+        with open(JsonFile, "r") as f:
+            st.write(json.load(f))
+    except FileNotFoundError:
+        st.error("User JSON file not found.")
+    except json.JSONDecodeError:
+        st.error("Error decoding user JSON file.")
 else:
     pass
-    
-
